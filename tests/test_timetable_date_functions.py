382061fadb3e4f1ded99215c1230b16e37d398bf
Relativity74205 <arkadius@schuchhardt.com>
2019-04-01 22:16:35 +0200

    added and tested timetable_date_functions

---
+++ tests/test_timetable_date_functions.py
@@ -0,0 +1,45 @@
+import pytest
+from datetime import datetime
+
+import timetable_date_functions as tdf
+
+
+@pytest.mark.parametrize("last_datehour, last_time", [
+    ('2019010101', datetime(2019, 1, 1, 1)),
+    ('2019010100', datetime(2019, 1, 1, 0)),
+    ('2019111', datetime(2019, 1, 1, 1))
+])
+def test_get_last_default_plan_time(last_datehour, last_time):
+    assert tdf.get_last_default_plan_time(last_datehour) == last_time
+
+
+@pytest.mark.parametrize("last_time, amount_missing_dates, missing_dates", [
+    (datetime(2019, 1, 1, 1), 1, [(2019, 1, 1, 2)]),
+    (datetime(2019, 1, 1, 1), 3, [(2019, 1, 1, 2),
+                                  (2019, 1, 1, 3),
+                                  (2019, 1, 1, 4)]),
+    (datetime(2019, 1, 1, 1), 0, [])
+])
+def test_generate_missing_dates(last_time, amount_missing_dates, missing_dates):
+    assert tdf.generate_missing_dates(last_time, amount_missing_dates) == missing_dates
+
+
+@pytest.mark.parametrize("missing_dates, missing_dates_tuple", [
+    ([datetime(2019, 1, 1, 1)], [(2019, 1, 1, 1)]),
+    ([datetime(2019, 1, 1, 1), datetime(2019, 1, 1, 2), datetime(2019, 1, 1, 3)],
+     [(2019, 1, 1, 1), (2019, 1, 1, 2), (2019, 1, 1, 3)]),
+    ([], [])
+])
+def test_convert_missing_dates(missing_dates, missing_dates_tuple):
+    assert tdf.convert_missing_dates(missing_dates) == missing_dates_tuple
+
+
+@pytest.mark.parametrize("current_time, last_datehour, missing_dates", [
+    (datetime(2019, 3, 15, 18, 12, 12), '2019031518', tdf.generate_missing_dates(datetime(2019, 3, 15, 18), 12)),
+    (datetime(2019, 3, 15, 18, 12, 12), '2019031519', tdf.generate_missing_dates(datetime(2019, 3, 15, 19), 11)),
+    (datetime(2019, 3, 15, 18, 12, 12), None, tdf.generate_missing_dates(datetime(2019, 3, 15, 17), 13))
+])
+def test_get_missing_default_plan_dates(current_time, last_datehour, missing_dates):
+    a = tdf.get_missing_default_plan_dates(current_time, last_datehour)
+    print(a)
+    assert a == missing_dates
---
+++ timetable_date_functions.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+from datetime import datetime, timedelta
+import math
+
+
+def get_missing_default_plan_dates(current_time: datetime, last_datehour: str, max_default_plans: int = 12) \
+        -> List[Tuple[int, int, int, int]]:
+    if last_datehour is not None:
+        last_time = get_last_default_plan_time(last_datehour)
+    else:
+        last_time = None
+
+    if last_time is not None:
+        time_delta = math.floor((last_time - current_time).total_seconds() / (60 * 60))
+        amount_missing_datehours = max_default_plans - time_delta - 1
+    else:
+        last_time = current_time.replace(microsecond=0, second=0, minute=0) - timedelta(hours=1)
+        amount_missing_datehours = max_default_plans + 1
+
+    missing_dates = generate_missing_dates(last_time, amount_missing_datehours)
+    return missing_dates
+
+
+def generate_missing_dates(last_time: datetime, amount_missing_dates: int) -> List[Tuple[int, int, int, int]]:
+    missing_dates = []
+    for i in range(amount_missing_dates):
+        default_plan_time = last_time + timedelta(hours=i + 1)
+        missing_dates.append(default_plan_time)
+
+    missing_dates_tuple = convert_missing_dates(missing_dates)
+
+    return missing_dates_tuple
+
+
+def convert_missing_dates(missing_dates: List[datetime]) -> List[Tuple[int, int, int, int]]:
+    missing_dates = [(missing_date.year,
+                      missing_date.month,
+                      missing_date.day,
+                      missing_date.hour) for missing_date in missing_dates]
+    return missing_dates
+
+
+def get_last_default_plan_time(last_datehour: str) -> datetime:
+    return datetime.strptime(last_datehour, '%Y%m%d%H')
