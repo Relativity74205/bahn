9af2b4dc4219466cf612d614c7bcc8909f3a9725
Relativity74205 <arkadius@schuchhardt.com>
2019-04-02 22:03:44 +0200

    changes to reflect introduction APIRequests file

--- main_manual.py
+++ main_manual.py
@@ -1,20 +1,23 @@
 import time
 import logging
+from typing import List
 
-import DatabaseConnection as dc
+import DatabaseConnection
 import BahnAPI
 import Timetable
+from TrainStop import TrainStop
+from TrainStopChange import TrainStopChange
 
 
 def main():
     set_logging_config()
-    db = dc.DatabaseConnection()
-    # reset_db(db)
+    db = DatabaseConnection.DatabaseConnection()
+    db.reset_db()
 
     ba = BahnAPI.BahnAPI()
 
-    timetable = Timetable.Timetable(ba, db)
-    get_default_time_table(timetable, 'Duisburg Hbf', 2019, 3, 30, 24)
+    timetable = Timetable.Timetable(ba)
+    timetables = get_default_time_table(timetable, 'Duisburg Hbf', 2019, 3, 30, 24)
     # get_changes(timetable, 'Duisburg Hbf', 'full')
     # get_changes(timetable, 'Duisburg Hbf', 'recent')
 
@@ -30,21 +33,26 @@ def reset_db(db):
     db.create_tables()
 
 
-def get_changes(timetable: Timetable, station: str, request_type: str = 'full'):
-    _ = timetable.get_changes(station=station, request_type=request_type)
+def get_changes(db: DatabaseConnection, timetable: Timetable,
+                station: str, request_type: str = 'full'):
+    train_stop_changes = timetable.get_changes(station=station, request_type=request_type)
+
+    db.save_bulk(train_stop_changes)
 
 
-def get_default_time_table(timetable: Timetable, station: str, year: int, month: int, day: int, hour: int):
-    _ = timetable.get_default_timetable(station=station,
-                                        year=year,
-                                        month=month,
-                                        day=day,
-                                        hour=hour)
+def get_default_time_table(db: DatabaseConnection, timetable: Timetable,
+                           station: str, year: int, month: int, day: int, hour: int):
+    timetables = timetable.get_default_timetable(station=station,
+                                                 year=year,
+                                                 month=month,
+                                                 day=day,
+                                                 hour=hour)
+    db.save_bulk(timetables)
 
 
 def get_bahnhof_dict(bahnapi: BahnAPI):
-    stations = ['Köln Hbf', 'Düsseldorf Hbf', 'Duisburg Hbf', 'Essen Hbf', 'Oberhausen Hbf', 'Bochum Hbf', 'Dortmund Hbf',
-                'Neuss Hbf', 'Krefeld Hbf', 'Mülheim (Ruhr) Hbf', 'Düsseldorf Flughafen']
+    stations = ['Köln Hbf', 'Düsseldorf Hbf', 'Duisburg Hbf', 'Essen Hbf', 'Oberhausen Hbf', 'Bochum Hbf',
+                'Dortmund Hbf', 'Neuss Hbf', 'Krefeld Hbf', 'Mülheim (Ruhr) Hbf', 'Düsseldorf Flughafen']
     bahnhof_dict = {}
     for station in stations:
         print(station)
