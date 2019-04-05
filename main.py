ae2387efefc55f85196ee0f5ab986779bcc146a7
Relativity74205 <arkadius@schuchhardt.com>
2019-04-05 21:42:32 +0200

    refactored main for APIRequests

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-import time
 import logging
 
 import APIRequests
-import config.config as config
 
 
 def main():
     set_logging_config()
-    api_requests = APIRequests.APIRequests()
 
-    # get_changes(db, timetable)
+    logging.info('Start App; initializing APIRequests')
+    api_requests = APIRequests.APIRequests()
+    logging.info('Start main loop!')
+    api_requests.main_loop()
 
 
 def set_logging_config():
     logging.basicConfig(filename='test_log.log',
                         format='%(asctime)s %(levelname)s: %(message)s',
                         level=logging.DEBUG)
-
-
-def get_bahnhof_dict(bahnapi: BahnAPI):
-    stations = config.stations
-    bahnhof_dict = {}
-    for station in stations:
-        abbrev = bahnapi.get_bahnhof_abbrev(station)
-        eva = bahnapi.get_eva_number(abbrev)
-        bahnhof_dict[station] = {'abbrev': abbrev,
-                                 'eva': eva}
-
-        # TODO sleeptime into config
-        time.sleep(6.5)
-
-    print(bahnhof_dict)
 
 
 if __name__ == '__main__':
