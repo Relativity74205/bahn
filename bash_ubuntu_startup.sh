da173ef9f50b8eaf8fec2f7a8a5b8b83f71b14cc
Relativity74205 <arkadius@schuchhardt.com>
2019-06-17 22:47:39 +0200

    asd

--- bash_ubuntu_startup.sh
+++ bash_ubuntu_startup.sh
@@ -5,8 +5,9 @@ sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev l
 sudo wget -P ~ "https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz"
 sudo tar xzf ~/Python-3.7.3.tgz -C ~
 cd ~/Python-3.7.3/
-sudo ./configure --enable-optimizations --enable-loadable-sqlite-extensions && sudo make && sudo make install
-#sudo ./configure --enable-loadable-sqlite-extensions && make && sudo make install
+#sudo ./configure --enable-optimizations --enable-loadable-sqlite-extensions && sudo make && sudo make install
+sudo ./configure --enable-loadable-sqlite-extensions && sudo make && sudo make install
 cd ~
 sudo rm ~/Python-3.7.3.tgz
 sudo rm -rf ~/Python-3.7.3/
+mkdir bahnscrapper
