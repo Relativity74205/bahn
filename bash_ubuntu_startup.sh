a7187393d702990a62cda09cfabfdf132adfd24d
Relativity74205 <arkadius@schuchhardt.com>
2019-06-17 21:40:07 +0200

    new version for ubuntu

--- bash_ubuntu_startup.sh
+++ bash_ubuntu_startup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-
-sudo apt-get install libsqlite3-dev
-sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-
-wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
-tar xzf Python-3.7.3.tgz
-
-cd Python-3.7.3/
-./configure --enable-optimizations --enable-loadable-sqlite-extensions && make && sudo make install
+sudo apt-get -y update
+sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget libsqlite3-dev
+sudo wget -P ~ "https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz"
+sudo tar xzf ~/Python-3.7.3.tgz -C ~
+cd ~/Python-3.7.3/
+sudo ./configure --enable-optimizations --enable-loadable-sqlite-extensions && sudo make && sudo make install
+#sudo ./configure --enable-loadable-sqlite-extensions && make && sudo make install
+cd ~
+sudo rm ~/Python-3.7.3.tgz
+sudo rm -rf ~/Python-3.7.3/
