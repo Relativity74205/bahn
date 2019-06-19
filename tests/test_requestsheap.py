7aaad86d3310e981487fb3eb4113ed39b6a8765c
Relativity74205 <arkadius@schuchhardt.com>
2019-06-19 21:03:55 +0200

    repaired tests

--- tests/test_requestsheap.py
+++ tests/test_requestsheap.py
@@ -1,14 +1,14 @@
 import pytest
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 
-import RequestsHeap
+from project import RequestsHeap
 
 
 @pytest.fixture
 def requests_heap():
     requests_heap = RequestsHeap.RequestsHeap()
-    requests_heap.event_lifetime = 0.1
+    requests_heap.seconds_event_lifetime = 0.1
     requests_heap.limit_requests_heap = 3
 
     return requests_heap
@@ -16,14 +16,14 @@ def requests_heap():
 
 def test_requests_heap(requests_heap):
     assert len(requests_heap.heap) == 0
-    assert requests_heap.event_lifetime
+    assert requests_heap.seconds_event_lifetime
     assert requests_heap.limit_requests_heap
 
 
 def test_requests_heap_append_event(requests_heap):
     requests_heap.append_event()
     assert len(requests_heap.heap) == 1
-    assert (requests_heap.heap[0] - datetime.now()).total_seconds() == requests_heap.event_lifetime
+    assert (requests_heap.heap[0] - datetime.now()).total_seconds() - requests_heap.seconds_event_lifetime < 0.01
     requests_heap.append_event()
     requests_heap.append_event()
     assert len(requests_heap.heap) == 3
@@ -48,7 +48,7 @@ def test_requests_heap_remove_expired_requests(requests_heap):
     requests_heap.append_event()
     requests_heap.append_event()
     assert len(requests_heap.heap) == 3
-    time.sleep(requests_heap.event_lifetime - 0.1 + 0.01)
+    time.sleep(requests_heap.seconds_event_lifetime - 0.1 + 0.01)
     requests_heap._remove_expired_requests()
     assert len(requests_heap.heap) == 2
     time.sleep(0.3)
